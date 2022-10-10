6debc7a43e99e9a07a06140af343742ee407fd72
binduvelivela <bindu.velivela0202@gmail.com>
2022-10-10 17:40:51 -0400

    Added code for routing and other functions

--- api/routes/courseRoutes.js
+++ api/routes/courseRoutes.js
@@ -1,10 +1,16 @@
-module.exports = function(app){
-	var coursesList = require('../controllers/courseControllers');
+const express = require("express");
+const router = express.Router();
+var coursesList = require("../controllers/courseControllers");
 
+// middleware that is specific to this router
+router.use((req, res, next) => {
+  next();
+});
 
-	//Course Routes
+router.get("/", coursesList.list_all_courses);
+router.post("/", coursesList.create_a_course);
+router.delete("/:ID", coursesList.delete_a_course);
+router.put("/:ID", coursesList.update_a_course);
+router.get("/:ID", coursesList.read_a_course);
 
-	app.route('/courses')
-	.get(coursesList.list_all_courses)
-	.post(coursesList.create_a_course);
-};
+module.exports = router;
--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-var course = require('./api/models/courseModel'),
-bodyParser = require('body-parser');
+var course = require("./api/models/courseModel"),
+  bodyParser = require("body-parser");
 const InitiateMongoServer = require("./db");
 
 InitiateMongoServer();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({
-	extended:true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(express.json());
 
-var routes = require('./api/routes/courseRoutes');//importing route
-routes(app);//register the route
+var routes = require("./api/routes/courseRoutes"); //importing route
+//routes(app); //register the route
+app.use("/courses", routes);
 
 // app.get("/",function(req,res) => {
 
